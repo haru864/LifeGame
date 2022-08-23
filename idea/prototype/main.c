2ea0dd2e0b5e4feaed5a411576fe77062cda871b
haru864 <kawachigenji95@gmail.com>
2022-08-24 01:55:34 +0900

    add function to move cursor

--- idea/prototype/main.c
+++ idea/prototype/main.c
@@ -1,40 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 // #include <ncurses.h>
 
 #define FIELD_WIDTH 24
 #define FIELD_HEIGHT 24
 
-int cursorX, cursorY;
+int cursorX = 0;
+int cursorY = 0;
 
 // 生存 -> #
 // 死亡 -> .
 
+bool moveCursor(char c);
+void checkCoordinate();
+
 int main()
 {
-    // 別のターミナル画面を開いて、そこにゲーム画面を表示させたい
-    // system("gnome-terminal");
+    // manコマンドみたいにターミナル画面を占有したい
 
-    for (int y = 0; y < FIELD_HEIGHT; y++)
+    while (true)
     {
-        for (int x = 0; x < FIELD_WIDTH; x++)
+        for (int y = 0; y < FIELD_HEIGHT; y++)
         {
-            if (x == cursorX && y == cursorY)
+            for (int x = 0; x < FIELD_WIDTH; x++)
             {
-                printf("#");
+                if (x == cursorX && y == cursorY)
+                {
+                    printf("#");
+                }
+                else
+                {
+                    printf(".");
+                }
             }
-            else
-            {
-                printf(".");
-            }
+            printf("\n");
         }
-        printf("\n");
+
+        printf("Push 'W', 'A', 'S', or 'D' to move your cursor.\n");
+        printf("If you want to quit game, please push 'Q'.\n");
+        printf("> ");
+        if (moveCursor((char)getchar()) == false)
+        {
+            break;
+        }
     }
 
-    // getch();
-    printf("If you want to quit game, please push RETURN button.");
-    int USER_INPUT = getchar();
     printf("Thank you for playing!\n");
 
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+bool moveCursor(char c)
+{
+    switch (c)
+    {
+    case 'w':
+        cursorY--;
+        break;
+    case 'a':
+        cursorX--;
+        break;
+    case 's':
+        cursorY++;
+        break;
+    case 'd':
+        cursorX++;
+        break;
+    case 'q':
+        return false;
+        break;
+    default:
+        printf("Push 'W', 'A', 'S', 'D', or 'Q'.\n");
+        break;
+    }
+
+    checkCoordinate();
+
+    return true;
+}
+
+void checkCoordinate()
+{
+    if (cursorX < 0)
+        cursorX = 0;
+    if (cursorX > FIELD_WIDTH)
+        cursorX = FIELD_WIDTH;
+    if (cursorY < 0)
+        cursorY = 0;
+    if (cursorY > FIELD_HEIGHT)
+        cursorY = FIELD_HEIGHT;
+    return;
 }
