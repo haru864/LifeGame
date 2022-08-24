18cd187e6dac2d21d34eb4229a9cce9720ac38af
haru864 <kawachigenji95@gmail.com>
2022-08-25 00:29:23 +0900

    [fix]range of motion of cursor fixed

--- idea/prototype/main.cpp
+++ idea/prototype/main.cpp
@@ -1,12 +1,15 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
 
-int SRC_WIDTH;
-int SRC_HEIGHT;
+using namespace std;
+
+int GAME_SCREEN_WIDTH;
+int GAME_SCREEN_HEIGHT;
 int cursorX = 0;
 int cursorY = 0;
-int COMMENT_ROW = 5;
+int COMMENT_ROW = 3;
 
 // 生存 -> #
 // 死亡 -> .
@@ -20,57 +23,39 @@ int main()
 
     int yMax, xMax;
     getmaxyx(stdscr, yMax, xMax);
-    SRC_HEIGHT = yMax - COMMENT_ROW;
-    SRC_WIDTH = xMax;
+    GAME_SCREEN_HEIGHT = yMax - COMMENT_ROW;
+    GAME_SCREEN_WIDTH = xMax;
 
     refresh();
 
-    for (int y = 0; y < SRC_HEIGHT; y++)
+    while (true)
     {
-        for (int x = 0; x < SRC_WIDTH; x++)
+        int y, x;
+        for (y = 0; y < GAME_SCREEN_HEIGHT; y++)
         {
-            if (x == cursorX && y == cursorY)
+            for (x = 0; x < GAME_SCREEN_WIDTH; x++)
             {
-                mvprintw(y, x, "#");
-            }
-            else
-            {
-                mvprintw(y, x, ".");
+                if (x == cursorX && y == cursorY)
+                {
+                    mvprintw(y, x, "#");
+                }
+                else
+                {
+                    mvprintw(y, x, ".");
+                }
             }
         }
+        mvprintw(GAME_SCREEN_HEIGHT + 0, 0, "Push 'W', 'A', 'S', or 'D' to move your cursor.");
+        mvprintw(GAME_SCREEN_HEIGHT + 1, 0, "If you want to quit game, please push 'Q'.");
+        mvprintw(GAME_SCREEN_HEIGHT + 2, 0, "> ");
+        if (moveCursor((char)getch()) == false)
+        {
+            break;
+        }
     }
-    getch();
-
-    // while (true)
-    // {
-    //     for (int y = 0; y < SRC_HEIGHT; y++)
-    //     {
-    //         for (int x = 0; x < SRC_WIDTH; x++)
-    //         {
-    //             move(y, x);
-    //             if (x == cursorX && y == cursorY)
-    //             {
-    //                 printw("#");
-    //             }
-    //             else
-    //             {
-    //                 printw(".");
-    //             }
-    //         }
-    //         printf("\n");
-    //     }
-    //     printf("Push 'W', 'A', 'S', or 'D' to move your cursor.\n");
-    //     printf("If you want to quit game, please push 'Q'.\n");
-    //     printf("> ");
-    //     if (moveCursor((char)getchar()) == false)
-    //     {
-    //         break;
-    //     }
-    // }
-
-    printf("Thank you for playing!\n");
 
     endwin();
+    printf("Thank you for playing!\n");
 
     return EXIT_SUCCESS;
 }
@@ -95,7 +80,7 @@ bool moveCursor(char c)
         return false;
         break;
     default:
-        printf("Push 'W', 'A', 'S', 'D', or 'Q'.\n");
+        // printf("Push 'W', 'A', 'S', 'D', or 'Q'.\n");
         break;
     }
 
@@ -108,11 +93,11 @@ void checkCoordinate()
 {
     if (cursorX < 0)
         cursorX = 0;
-    if (cursorX > SRC_WIDTH)
-        cursorX = SRC_WIDTH;
+    if (cursorX >= GAME_SCREEN_WIDTH)
+        cursorX = GAME_SCREEN_WIDTH - 1;
     if (cursorY < 0)
         cursorY = 0;
-    if (cursorY > SRC_HEIGHT)
-        cursorY = SRC_HEIGHT;
+    if (cursorY >= GAME_SCREEN_HEIGHT)
+        cursorY = GAME_SCREEN_HEIGHT - 1;
     return;
 }
